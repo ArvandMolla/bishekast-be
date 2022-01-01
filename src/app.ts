77a67f25993cf702107e6e5fd2aca1ee7904a987
Arvand Molla <mr.arvand@gmail.com>
2022-01-01 18:33:04 +0100

    server boiler plate

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express, { Application } from "express";
+import cors from "cors";
+import { createServer } from "http";
+
+process.env.TS_NODE_DEV && require("dotenv").config();
+
+const app: Application = express();
+
+const whiteList = [
+  process.env.FRONTEND_DEV_URL,
+  process.env.FRONTEND_CLOUD_URL,
+];
+const corsOptions = {
+  origin: function (origin: any, next: any) {
+    console.log("ORIGIN ", origin);
+    if (whiteList.indexOf(origin) !== -1) {
+      next(null, true);
+    } else {
+      next(new Error("CORS TROUBLES!!!!!"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.get("/test", (req, res) => {
+  res.status(200).send("hello");
+});
+
+const server = createServer(app);
+
+export default server;
